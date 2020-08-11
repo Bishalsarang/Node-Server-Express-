1dc5bcc5defb3545042beee24330613a71b50d91
Bishalsarang <bishalsarang@gmail.com>
2020-08-11 22:45:12 +0545

    Remove unnecessary files: fileHandler.js and server.js

--- src/fileOperation.js
+++ src/fileOperation.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 
+function handlePromise(reject, resolve) {
+  return (err, done) => (err ? reject(err) : resolve(done));
+}
+
 /**
  *
  * @param {String} filePath
  * @param {String} content
+ * @returns {Promise}
  */
 const write = (filePath, content) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, content, (err, done) => (err ? reject(err) : resolve(done)));
+    fs.writeFile(filePath, content, handlePromise(reject, resolve));
   });
 };
 
@@ -15,10 +20,11 @@ const write = (filePath, content) => {
  *
  * @param {String} filePath
  * @param {String} encoding
+ * @returns {Promise}
  */
 const read = (filePath, encoding = 'UTF-8') => {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, encoding, (err, done) => (err ? reject(err) : resolve(done)));
+    fs.readFile(filePath, encoding, handlePromise(reject, resolve));
   });
 };
 
@@ -26,20 +32,22 @@ const read = (filePath, encoding = 'UTF-8') => {
  *
  * @param {String} filePath
  * @param {String} newFilePath
+ * @returns {Promise}
  */
 const rename = (filePath, newFilePath) => {
   return new Promise((resolve, reject) => {
-    fs.rename(filePath, newFilePath, (err, done) => (err ? reject(err) : resolve(done)));
+    fs.rename(filePath, newFilePath, handlePromise(reject, resolve));
   });
 };
 
 /**
  *
  * @param {String} filePath
+ * @returns {Promise}
  */
 const del = (filePath) => {
   return new Promise((resolve, reject) => {
-    fs.unlink(filePath, (err, done) => (err ? reject(err) : resolve(done)));
+    fs.unlink(filePath, handlePromise(reject, resolve));
   });
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const env = require('./env');
-
 const express = require('express');
 const app = express();
+
+const env = require('./env');
+
+const fileOperation = require('./fileOperation');
 
 const PORT = process.env.PORT || 3003;
 const HOST = process.env.HOST || 'localhost';
@@ -9,8 +11,56 @@ const HOST = process.env.HOST || 'localhost';
 app.set('port', PORT);
 app.set('host', HOST);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.end(
+    `Please try following path for file operations:
+       1. read: /read/<fileName>
+       2. write: /write/<fileName>/<content>
+       3. rename: /rename/<oldFileName>/<newFileName>
+       4. delete: /del/<fileName>`,
+  );
+});
 
-app.listen(app.get('port'), app.get('host'), () =>
-  console.log(`Listening on ${app.get('host')}:${app.get('port')} \nPress CTRL + C to exit server`),
-);
+app.get('/read/:fileName', (req, res) => {
+  const { fileName } = req.params;
+
+  fileOperation
+    .read(fileName)
+    .then((data) => res.end(`File read successfully ${fileName} \n Content \n ${data}`))
+    .catch((err) => res.end(`Failed to read file ${err}`));
+});
+
+app.get('/write/:fileName/:content', (req, res) => {
+  const { fileName, content } = req.params;
+
+  fileOperation
+    .write(fileName, content)
+    .then(() => res.end(`File successfully written ${fileName}`))
+    .catch((err) => res.end('Failed to write file' + err));
+});
+
+app.get('/rename/:oldFileName/:newFileName', (req, res) => {
+  const { oldFileName, newFileName } = req.params;
+
+  fileOperation
+    .rename(oldFileName, newFileName)
+    .then(() => res.end(`File renamed successfully from ${oldFileName} to ${newFileName}`))
+    .catch((err) => res.end('Failed to rename file' + err));
+});
+
+app.get('/delete/:fileName', (req, res) => {
+  const { fileName } = req.params;
+
+  fileOperation
+    .del(fileName)
+    .then(() => res.end(`File deleted successfully ${fileName}`))
+    .catch((err) => res.end('Failed to delete file ' + err));
+});
+
+app.listen(app.get('port'), app.get('host'), (err) => {
+  if (err) {
+    console.log('Error creating server');
+  } else {
+    console.log(`Listening on ${app.get('host')}:${app.get('port')} \nPress CTRL + C to exit server`);
+  }
+});
