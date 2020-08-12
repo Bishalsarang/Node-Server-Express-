7c010803a08f12a78bbfd27299484f4ec4d470bd
Bishalsarang <bishalsarang@gmail.com>
2020-08-12 10:29:45 +0545

    Move routes to routes.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 
-const env = require('./env');
+let routes = require('./routes');
 
-const constants = require('./constants');
-const fileOperation = require('./fileOperation');
+const env = require('./env');
 
 const PORT = process.env.PORT || 3003;
 const HOST = process.env.HOST || 'localhost';
@@ -12,97 +11,7 @@ const HOST = process.env.HOST || 'localhost';
 app.set('port', PORT);
 app.set('host', HOST);
 
-app.get('/', (req, res) => {
-  res.end(
-    `Please try following path for file operations:
-       1. read: /read/<fileName>
-       2. write: /write/<fileName>/<content>
-       3. rename: /rename/<oldFileName>/<newFileName>
-       4. delete: /del/<fileName>`,
-  );
-});
-
-app.get('/read/:fileName', async (req, res) => {
-  const { fileName } = req.params;
-  const { success: successMessage, fail: failedMessage } = constants.MESSAGES.read;
-
-  try {
-    const data = await fileOperation.read(fileName);
-
-    res.status(200).json({
-      data,
-      fileName,
-      success: true,
-      message: successMessage,
-    });
-  } catch (err) {
-    throw res.json({
-      success: false,
-      message: failedMessage + err,
-    });
-  }
-});
-
-app.get('/write/:fileName/:content', async (req, res) => {
-  const { fileName, content } = req.params;
-  const { success: successMessage, fail: failedMessage } = constants.MESSAGES.write;
-
-  try {
-    await fileOperation.write(fileName, content);
-
-    res.status(200).json({
-      fileName,
-      success: true,
-      message: successMessage,
-    });
-  } catch (err) {
-    throw res.json({
-      success: false,
-      message: failedMessage + err,
-    });
-  }
-});
-
-app.get('/rename/:oldFileName/:newFileName', async (req, res) => {
-  const { oldFileName, newFileName } = req.params;
-  const { success: successMessage, fail: failedMessage } = constants.MESSAGES.rename;
-
-  try {
-    await fileOperation.rename(oldFileName, newFileName);
-
-    res.json({
-      oldFileName,
-      newFileName,
-      success: true,
-      message: successMessage,
-    });
-  } catch (err) {
-    throw res.json({
-      success: false,
-      message: failedMessage + err,
-    });
-  }
-});
-
-app.get('/delete/:fileName', async (req, res) => {
-  const { fileName } = req.params;
-  const { success: successMessage, fail: failedMessage } = constants.MESSAGES.read;
-
-  try {
-    await fileOperation.del(fileName);
-
-    res.json({
-      fileName,
-      success: true,
-      message: successMessage,
-    });
-  } catch (err) {
-    throw res.json({
-      success: false,
-      message: failedMessage + err,
-    });
-  }
-});
+app.use('/', routes);
 
 app.listen(app.get('port'), app.get('host'), (err) => {
   if (err) {
